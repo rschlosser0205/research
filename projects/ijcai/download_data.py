ec6449b53b7faa1246704716d36b5d43c65e1e04
Justin Li <justinnhli@gmail.com>
2019-03-18 17:02:43 -0700

    refactor download script

--- projects/ijcai/download_data.py
+++ projects/ijcai/download_data.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 from collections import namedtuple
-from itertools import combinations, chain
+from itertools import chain
 
 from networkx import MultiDiGraph, DiGraph
 from networkx.algorithms import shortest_path
@@ -12,10 +12,12 @@ from research.rl_memory import SparqlKB
 
 from record_store import first_letter, date_to_decade
 
-SparqlGraph = namedtuple('SparqlGraph', 'name, graph, props, start_vars, end_var, diff_vars, transform')
+Experiment = namedtuple('Experiment', 'name, edges, start_vars, end_var, transform')
+SparqlGraph = namedtuple('SparqlGraph', 'name, graph, start_vars, end_var, transform, actions')
 Action = namedtuple('Action', 'type, subject, property, object, result')
 
 LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 KB_SOURCE = SparqlEndpoint('http://162.233.132.179:8890/sparql')
 KB_ADAPTOR = SparqlKB(KB_SOURCE)
@@ -24,81 +26,21 @@ NAME_PROP = '<http://xmlns.com/foaf/0.1/name>'
 RELEASE_DATE_PROP = '<http://wikidata.dbpedia.org/ontology/releaseDate>'
 ALBUM_PROP = '<http://wikidata.dbpedia.org/ontology/album>'
 ARTIST_PROP = '<http://wikidata.dbpedia.org/ontology/artist>'
-HOMETOWN_PROP = 'http://wikidata.dbpedia.org/ontology/hometown'
-COUNTRY_PROP = 'http://wikidata.dbpedia.org/ontology/country'
+HOMETOWN_PROP = '<http://wikidata.dbpedia.org/ontology/hometown>'
+COUNTRY_PROP = '<http://wikidata.dbpedia.org/ontology/country>'
 
 
-def release_date():
+def to_sparql_graph(name, edges, start_vars, end_var, transform=None):
     graph = MultiDiGraph()
-    graph.add_edge('track', 'album_uri', label=ALBUM_PROP)
-    graph.add_edge('album_uri', 'album_name', label=NAME_PROP)
-    graph.add_edge('album_uri', 'release_date', label=RELEASE_DATE_PROP)
+    for subj, prop, obj in edges:
+        graph.add_edge(subj, obj, prop=prop)
     return SparqlGraph(
-        'album_date',
+        name,
         graph,
-        [NAME_PROP],
-        ['album_name'],
-        'release_date',
-        [],
-        date_to_decade,
-    )
-
-
-def artist():
-    graph = MultiDiGraph()
-    graph.add_edge('track_uri', 'album_uri', label=ALBUM_PROP)
-    graph.add_edge('album_uri', 'album_name', label=NAME_PROP)
-    graph.add_edge('album_uri', 'artist_uri', label=ARTIST_PROP)
-    graph.add_edge('artist_uri', 'artist_name', label=NAME_PROP)
-    return SparqlGraph(
-        'album_artist',
-        graph,
-        [NAME_PROP],
-        ['album_name'],
-        'artist_name',
-        [],
-        first_letter,
-    )
-
-
-def country():
-    graph = MultiDiGraph()
-    graph.add_edge('track_uri', 'album_uri', label=ALBUM_PROP)
-    graph.add_edge('album_uri', 'album_name', label=NAME_PROP)
-    graph.add_edge('album_uri', 'artist_uri', label=ARTIST_PROP)
-    graph.add_edge('artist_uri', 'hometown_uri', label=HOMETOWN_PROP)
-    graph.add_edge('hometown_uri', 'country_uri', label=COUNTRY_PROP)
-    graph.add_edge('country_uri', 'country_name', label=NAME_PROP)
-    return SparqlGraph(
-        'album_country',
-        graph,
-        [NAME_PROP],
-        ['album_name'],
-        'country_name',
-        [],
-        first_letter,
-    )
-
-
-def other_album():
-    graph = MultiDiGraph()
-    graph.add_edge('track', 'album_uri', label=ALBUM_PROP)
-    graph.add_edge('album_uri', 'album_name', label=NAME_PROP)
-    graph.add_edge('album_uri', 'release_date', label=RELEASE_DATE_PROP)
-    graph.add_edge('album_uri', 'artist_uri', label=ARTIST_PROP)
-    graph.add_edge('other_track', 'other_album_uri', label=ALBUM_PROP)
-    graph.add_edge('other_album_uri', 'other_album_name', label=NAME_PROP)
-    graph.add_edge('other_album_uri', 'other_release_date', label=RELEASE_DATE_PROP)
-    graph.add_edge('other_album_uri', 'artist_uri', label=ARTIST_PROP)
-    graph.add_edge('artist_uri', 'artist_name', label=NAME_PROP)
-    return SparqlGraph(
-        'album_date_album',
-        graph,
-        [NAME_PROP, RELEASE_DATE_PROP],
-        ['album_name', 'other_release_date'],
-        'other_album_name',
-        ['album_uri', 'other_album_uri'],
-        first_letter,
+        start_vars,
+        end_var,
+        transform,
+        graph_to_actions(graph, start_vars, end_var),
     )
 
 
@@ -115,25 +57,29 @@ def networkx_to_sparql(sparql_graph):
         variables.add(src)
         variables.add(dst)
         for _, edge_data in sparql_graph.graph.get_edge_data(src, dst).items():
-            if edge_data["label"] == NAME_PROP:
+            if edge_data['prop'] == NAME_PROP:
                 names.add(dst)
-            lines.append(f'    ?{src} {edge_data["label"]} ?{dst} .')
-    for var in sparql_graph.start_vars + [sparql_graph.end_var] + sparql_graph.diff_vars:
-        if var not in variables:
-            raise ValueError(f'?{var} is not a variable in the query')
+            lines.append(f'    ?{src} {edge_data["prop"]} ?{dst} .')
     for name in names:
         lines.append(f'    FILTER ( lang(?{name}) = "en" )')
-    for var1, var2 in combinations(sparql_graph.diff_vars, 2):
-        lines.append(f'    FILTER ( ?{var1} != ?{var2} )')
     lines.append(f'}}')
     return '\n'.join(lines)
 
 
-def get_edge_label(graph, src, dst):
-    return graph.get_edge_data(src, dst)[0]['label']
+def is_unambiguous(name):
+    query = f'''
+        SELECT DISTINCT ?concept WHERE {{
+            ?concept {NAME_PROP} {name}
+        }} LIMIT 2
+    '''
+    return len(list(KB_SOURCE.query_sparql(query))) == 1
 
 
-def add_path_to_action_graph(path, graph, action_graph):
+def get_edge_prop(graph, src, dst):
+    return graph.get_edge_data(src, dst)[0]['prop']
+
+
+def add_path_to_action_graph(graph, action_graph, path):
     i = 0
     while i < len(path) - 1:
         curr_node = path[i]
@@ -149,59 +95,59 @@ def add_path_to_action_graph(path, graph, action_graph):
         )
         if next_node == path[-1]:
             # use child
-            label = get_edge_label(graph, curr_node, next_node)
+            prop = get_edge_prop(graph, curr_node, next_node)
             action_graph.add_edge(
                 curr_node, next_node,
-                action=Action('use', curr_node, label, next_node, next_node),
-                label=f'use child: {curr_node} -> {next_node}',
+                action=Action('use', curr_node, prop, next_node, next_node),
             )
             i += 1
         elif graph.has_edge(next_node, curr_node):
             # query self
-            label = get_edge_label(graph, next_node, curr_node)
+            prop = get_edge_prop(graph, next_node, curr_node)
             action_graph.add_edge(
                 curr_node, next_node,
-                action=Action('query', '_environment', label, curr_node, next_node),
-                label=f'query self: {next_node} -> {curr_node}',
+                action=Action('query', '_environment', prop, curr_node, next_node),
             )
             i += 1
         elif should_query_child:
             # query child
-            label = get_edge_label(graph, curr_node, next_node)
+            prop = get_edge_prop(graph, curr_node, next_node)
             action_graph.add_edge(
                 curr_node, next_next_node,
-                action=Action('query', curr_node, label, next_node, next_next_node),
-                label=f'query child: {curr_node} -> {next_next_node}',
+                action=Action('query', curr_node, prop, next_node, next_next_node),
             )
             i += 2
         elif graph.has_edge(curr_node, next_node):
             # retrieve child
-            label = get_edge_label(graph, curr_node, next_node)
+            prop = get_edge_prop(graph, curr_node, next_node)
             action_graph.add_edge(
                 curr_node, next_node,
-                action=Action('retrieve', curr_node, label, next_node, next_node),
-                label=f'retrieve child: {curr_node} -> {next_node}',
+                action=Action('retrieve', curr_node, prop, next_node, next_node),
             )
             i += 1
         else:
             ValueError(f'not sure what to do at {curr_node}')
 
 
-def build_action_graph(sparql_graph):
+def graph_to_actions(graph, start_vars, end_var):
+    action_graph = build_action_graph(graph, start_vars, end_var)
+    return sequentialize_actions(start_vars, end_var, action_graph)
+
+
+def build_action_graph(graph, start_vars, end_var):
     action_graph = DiGraph()
-    graph = sparql_graph.graph
-    for start_var in sparql_graph.start_vars:
-        path = shortest_path(graph.to_undirected(), start_var, sparql_graph.end_var)
-        add_path_to_action_graph(path, graph, action_graph)
+    for start_var in start_vars:
+        path = shortest_path(graph.to_undirected(), start_var, end_var)
+        add_path_to_action_graph(graph, action_graph, path)
     return action_graph
 
 
-def sequentialize_actions(sparql_graph, action_graph):
+def sequentialize_actions(start_vars, end_var, action_graph):
     queue = list(chain(*(
         action_graph.successors(start_var)
-        for start_var in sparql_graph.start_vars
+        for start_var in start_vars
     )))
-    visited = set(sparql_graph.start_vars)
+    visited = set(start_vars)
     actions = []
     while queue:
         curr_node = queue.pop(0)
@@ -213,33 +159,47 @@ def sequentialize_actions(sparql_graph, action_graph):
                 action_graph.get_edge_data(predecessor, curr_node)['action']
                 for predecessor in action_graph.predecessors(curr_node)
             ])
-            if curr_node == sparql_graph.end_var:
+            if curr_node == end_var:
                 break
             successors = list(action_graph.successors(curr_node))
             curr_node = successors[0]
             queue.extend(successors[1:])
-        if curr_node != sparql_graph.end_var:
+        if curr_node != end_var:
             queue.append(curr_node)
     return actions
 
 
-def download_qa_pairs(sparql_graph):
+def all_sparql_results(query_template):
     limit = 100
     offset = 0
-    query_template = networkx_to_sparql(sparql_graph)
     query = query_template + f' LIMIT {limit} OFFSET {offset}'
     results = KB_SOURCE.query_sparql(query)
     while results:
-        for result in results:
-            question = {
-                prop: result[start_var].rdf_format for prop, start_var
-                in zip(sparql_graph.props, sparql_graph.start_vars)
-            }
-            answer = result[sparql_graph.end_var].rdf_format
-            yield question, answer
+        yield from results
         offset += limit
         query = query_template + f' LIMIT {limit} OFFSET {offset}'
         results = KB_SOURCE.query_sparql(query)
+
+
+def download_qa_pairs(sparql_graph):
+    properties = dict(
+        (
+            start_var,
+            set(
+                edge[2] for edge
+                in sparql_graph.graph.in_edges(start_var, data='prop')
+            ),
+        )
+        for start_var in sparql_graph.start_vars
+    )
+    for result in all_sparql_results(networkx_to_sparql(sparql_graph)):
+        answer = (result[sparql_graph.end_var].rdf_format, )
+        question = set()
+        for start_var in sparql_graph.start_vars:
+            start_rdf = result[start_var].rdf_format
+            for prop in properties[start_var]:
+                question.add((prop, start_rdf))
+        yield question, answer
 
 
 def check_query(cache, actions):
@@ -261,11 +221,11 @@ def check_retrieve(cache, actions):
     return KB_ADAPTOR.retrieve(cache[action.subject][action.property])
 
 
-def is_valid_qa_pair(question, answer, sparql_graph, actions):
+def is_answerable(question, answer, sparql_graph):
     cache = {
-        '_environment': question,
+        '_environment': dict(question),
     }
-    for step, step_actions in enumerate(actions, start=1):
+    for step, step_actions in enumerate(sparql_graph.actions, start=1):
         LOGGER.debug(f'Step {step}')
         LOGGER.debug('\n'.join(f'    {action}' for action in step_actions))
         assert len(set(action.type for action in step_actions)) == 1
@@ -281,7 +241,7 @@ def is_valid_qa_pair(question, answer, sparql_graph, actions):
             result = check_retrieve(cache, step_actions)
         elif action.type == 'use':
             LOGGER.debug(f'checking {cache[action.subject].get(action.property, None)} == {answer}')
-            return cache[action.subject].get(action.property, None) == answer
+            return cache[action.subject].get(action.property, None) == answer[0]
         else:
             raise ValueError(step_actions)
         LOGGER.debug(f'result: {result}')
@@ -291,36 +251,126 @@ def is_valid_qa_pair(question, answer, sparql_graph, actions):
     raise ValueError('ran out of actions before use')
 
 
+def is_valid_qa(sparql_graph, question, answer):
+    return (
+        all(
+            prop != NAME_PROP or is_unambiguous(obj)
+            for prop, obj in question
+        )
+        and (
+            sparql_graph.transform is None
+            or sparql_graph.transform(answer[0]) is not None
+        )
+        and is_answerable(question, answer, sparql_graph)
+    )
+
+
+def get_valid_data(sparql_graph):
+    qas = {}
+    total = 0
+    valid = 0
+    for question, answer in download_qa_pairs(sparql_graph):
+        total += 1
+        LOGGER.info(f'verifying {question} -> {answer}')
+        if is_valid_qa(sparql_graph, question, answer):
+            if sparql_graph.transform is not None:
+                answer = sparql_graph.transform(answer[0])
+            q_list = tuple(sorted(question))
+            assert qas.get(q_list, None) in (None, answer)
+            if q_list not in qas:
+                yield question, answer
+                valid += 1
+            qas[q_list] = answer
+            if valid % 100 == 0:
+                LOGGER.info(f'processed {valid} valid albums out of {total} albums')
+                break
+
+
 def download_data(sparql_graph):
-    action_graph = build_action_graph(sparql_graph)
-    actions = sequentialize_actions(sparql_graph, action_graph)
     with open('data/' + sparql_graph.name, 'w') as fd:
         fd.write('(\n')
-    count = 0
-    for question, answer in download_qa_pairs(sparql_graph):
-        LOGGER.info(f'verifying {question} -> {answer}')
-        if is_valid_qa_pair(question, answer, sparql_graph, actions):
-            transformed_answer = sparql_graph.transform(answer)
-            if transformed_answer:
-                with open('data/' + sparql_graph.name, 'a') as fd:
-                    fd.write('    ' + repr((question, transformed_answer)))
-                    fd.write(',\n')
-                count += 1
-                if count % 100 == 0:
-                    LOGGER.debug(f'processed {count} albums')
-    with open('data/' + sparql_graph.name, 'a') as fd:
+        for qa_tuple in get_valid_data(sparql_graph):
+            fd.write(f'    {repr(qa_tuple)},\n')
         fd.write(')\n')
+
+
+def find_leaves(graph):
+    leaves = []
+    for node in graph.nodes:
+        try:
+            next(iter(graph.successors(node)))
+        except StopIteration:
+            leaves.append(node)
+    return leaves
+
+
+EXP_RELEASE_DATE = to_sparql_graph(
+    'album_date',
+    [
+        ('track', ALBUM_PROP, 'album_uri'),
+        ('album_uri', NAME_PROP, 'album_name'),
+        ('album_uri', RELEASE_DATE_PROP, 'release_date'),
+    ],
+    ['album_name'],
+    'release_date',
+    date_to_decade,
+)
+
+
+EXP_ARTIST = to_sparql_graph(
+    'album_artist',
+    (
+        ('track_uri', ALBUM_PROP, 'album_uri'),
+        ('album_uri', NAME_PROP, 'album_name'),
+        ('album_uri', ARTIST_PROP, 'artist_uri'),
+        ('artist_uri', NAME_PROP, 'artist_name'),
+    ),
+    ['album_name'],
+    'artist_name',
+    first_letter,
+)
+
+EXP_COUNTRY = to_sparql_graph(
+    'album_country',
+    (
+        ('track_uri', ALBUM_PROP, 'album_uri'),
+        ('album_uri', NAME_PROP, 'album_name'),
+        ('album_uri', ARTIST_PROP, 'artist_uri'),
+        ('artist_uri', HOMETOWN_PROP, 'hometown_uri'),
+        ('hometown_uri', COUNTRY_PROP, 'country_uri'),
+        ('country_uri', NAME_PROP, 'country_name'),
+    ),
+    ['album_name'],
+    'country_name',
+)
+
+
+EXP_OTHER_ALBUM = to_sparql_graph(
+    'album_date_album',
+    (
+        ('track', ALBUM_PROP, 'album_uri'),
+        ('album_uri', NAME_PROP, 'album_name'),
+        ('album_uri', ARTIST_PROP, 'artist_uri'),
+        ('other_track', ALBUM_PROP, 'other_album_uri'),
+        ('other_album_uri', NAME_PROP, 'other_album_name'),
+        ('other_album_uri', RELEASE_DATE_PROP, 'other_release_date'),
+        ('other_album_uri', ARTIST_PROP, 'artist_uri'),
+    ),
+    ['album_name', 'other_release_date'],
+    'other_album_name',
+    first_letter,
+)
 
 
 def main():
     if sys.argv[1] == 'release-date':
-        sparql_graph = release_date()
+        sparql_graph = EXP_RELEASE_DATE
     elif sys.argv[1] == 'artist':
-        sparql_graph = artist()
+        sparql_graph = EXP_ARTIST
     elif sys.argv[1] == 'country':
-        sparql_graph = country()
+        sparql_graph = EXP_COUNTRY
     elif sys.argv[1] == 'other-album':
-        sparql_graph = other_album()
+        sparql_graph = EXP_OTHER_ALBUM
     download_data(sparql_graph)
 
 
