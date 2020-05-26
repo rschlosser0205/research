1bc71d732fc98dbb05c3c58f6d5ae2a5b118ec52
Ruth Schlosser <rschlosser0205@gmail.com>
2020-05-26 12:46:34 -0700

    more for loop maintenance

--- research/Model_Generation.py
+++ research/Model_Generation.py
@@ -9,34 +9,37 @@ class Model_generation:
         self.act_capped = act_capped
         self.backlinks = backlinks
         self.representation = representation
-        act_class = Activation_Class(act_decay_rate, act_scale_factor, act_max_steps, act_capped)
-        store = NetworkXKB(act_class)
 
 
 
     # FIXME add a function that takes in a store and the rep parameters. this function will create the graph
     # where does this function go? in here
     # this function will accomplish store.store(...) * 4 or whatever
-    def create_graph(self, store):
+    def create_graph(self, store, backlinks):
         if self.representation == 1:
             # one level (?) representation
-            store.store(0, 'A', goes_to='B')
-            store.store(0, 'A', goes_to='D')
-            store.store(0, 'C', goes_to='B')
-            store.store(0, 'C', goes_to='D')
+            store.store(0, backlinks, 'A', goes_to='B')
+            store.store(0, backlinks, 'A', goes_to='D')
+            store.store(0, backlinks, 'C', goes_to='B')
+            store.store(0, backlinks, 'C', goes_to='D')
         elif self.representation == 2:
             # two-level representation
-            store.store(0, 'AB', first='A', second='B')
-            store.store(1, 'AD', first='A', second='D')
-            store.store(1, 'CB', first='C', second='B')
-            store.store(1, 'CD', first='C', second='D')
+            store.store(0, backlinks, 'AB', first='A', second='B')
+            store.store(1, backlinks, 'AD', first='A', second='D')
+            store.store(1, backlinks, 'CB', first='C', second='B')
+            store.store(1, backlinks, 'CD', first='C', second='D')
         elif self.representation == 3:
             # three levels (type=pairs is a shared child of AB and CD)
             # FIXME add other representations
+            store.store(0, backlinks, 'AB', first='A', second='B', type='pairs')
+            store.store(1, backlinks, 'AD', first='A', second='D', type='pairs')
+            store.store(1, backlinks, 'CB', first='C', second='B', type='pairs')
+            store.store(1, backlinks, 'CD', first='C', second='D', type='pairs')
 
     def test_model(self):
         # play with the parameters of activation class (decay_rate, scale_factor, max_steps, capped)
-        # things to include-- changes in directionality? BACKLINKS VS NONE
+        # things to include-- BACKLINKS VS NONE
+        # loops
 
         for rate in self.act_decay_rate:
             for scale in self.act_scale_factor:
@@ -44,7 +47,7 @@ class Model_generation:
                     for cap in self.act_capped:
                         for link in self.backlinks:
                             for rep in self.representation:
-                                store = NetworkXKB(Activation_Class(self.act_decay_rate, self.act_scale_factor, self.act_max_steps, self.act_capped))
+                                store = NetworkXKB(Activation_Class(rate, scale, step, cap))
                                 self.create_graph(store)
 
 
