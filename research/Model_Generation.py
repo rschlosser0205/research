a1d5d95a326a1f796a5c400a0663109ded2879e6
Ruth Schlosser <rschlosser0205@gmail.com>
2020-05-26 14:40:45 -0700

    start of query test

--- research/Model_Generation.py
+++ research/Model_Generation.py
@@ -12,16 +12,15 @@ class Model_generation:
 
 
 
-    # FIXME add a function that takes in a store and the rep parameters. this function will create the graph
-    # where does this function go? in here
-    # this function will accomplish store.store(...) * 4 or whatever
+    # comment out the two unused pairs!
     def create_graph(self, store, backlinks, representation):
         if representation == 1:
             # one level (?) representation
             store.store(0, backlinks, 'A', goes_to='B')
-            store.store(0, backlinks, 'A', goes_to='D')
-            store.store(0, backlinks, 'C', goes_to='B')
-            store.store(0, backlinks, 'C', goes_to='D')
+            store.store(1, backlinks, 'A', goes_to='D')
+            store.store(1, backlinks, 'C', goes_to='B')
+            store.store(1, backlinks, 'C', goes_to='D')
+
         elif representation == 2:
             # two-level representation
             store.store(0, backlinks, 'AB', first='A', second='B')
@@ -30,11 +29,24 @@ class Model_generation:
             store.store(1, backlinks, 'CD', first='C', second='D')
         elif representation == 3:
             # three levels (type=pairs is a shared child of AB and CD)
-            # FIXME add other representations
             store.store(0, backlinks, 'AB', first='A', second='B', type='pairs')
             store.store(1, backlinks, 'AD', first='A', second='D', type='pairs')
             store.store(1, backlinks, 'CB', first='C', second='B', type='pairs')
             store.store(1, backlinks, 'CD', first='C', second='D', type='pairs')
+
+    # FIXME refactor?
+    def query_test(self, representation, store):
+        # get from A to B in each representation and then print out the result
+        if representation == 1:
+            result = store.query(2, {'goes_to': 'B'})
+        elif representation == 2:
+            result = store.query(2, {'first': 'A'}) # this should give AB or AC
+            # FIXME how to get 'second' term?
+        elif representation == 3:
+            result = store.query(2, {'first': 'A'})  # this should give AB or AC
+            # FIXME how to get 'second' term?
+        # FIXME make this work so u can print ids
+        print(result)
 
     def test_model(self):
         # play with the parameters of activation class (decay_rate, scale_factor, max_steps, capped)
@@ -53,12 +65,14 @@ class Model_generation:
 
 
 
+
+
+
         # FIXME add queries and retrievals to get from A to B in all representations
         # FIXME add node id as outgoing edge/attribute
 
 
-        # get from A to B in rep 1
-        store.query(1, {'goes_to': 'B'})
+
 
 test = Model_generation([-0.5, -0.25], [0.5, 0.25], [2, 3], [True, False], [True, False], [1, 2, 3])
 test.test_model()
