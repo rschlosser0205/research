af788b3e0fc0dd50d1469278abc5e508074b7d28
Ruth Schlosser <rschlosser0205@gmail.com>
2020-06-05 11:39:27 -0700

    additional measures of fok
    incoming, outgoing, and total edges of the cue node

--- research/Model_Generation.py
+++ research/Model_Generation.py
@@ -9,8 +9,15 @@ def determine_fok_function(method):
     if method == 'cue':
         return cue_fok
     elif method == 'target':
-        # return the activation of what was returned by the query
         return target_fok
+    elif method == 'cue and target':
+        return cue_and_target_fok
+    elif method == 'incoming edges':
+        return incoming_edges_fok
+    elif method == 'outgoing edges':
+        return outgoing_edges_fok
+    elif method == 'total edges':
+        return total_edges_fok
 
 
 
@@ -18,12 +25,20 @@ def determine_fok_function(method):
 def cue_fok(store, cue, target, query_time):
     return store.get_activation(cue, (query_time - 0.0001))
 
-
-
-
 def target_fok(store, cue, target, query_time):
     return store.get_activation(target, (query_time - 0.0001))
 
+def cue_and_target_fok(store, cue, target, query_time):
+    return 0.5*(store.get_activation(target, (query_time - 0.0001)) + store.get_activation(cue, (query_time - 0.0001)))
+
+def incoming_edges_fok(store, cue, target, query_time):
+    return len(store.graph.in_edges(cue))
+
+def outgoing_edges_fok(store, cue, target, query_time):
+    return len(store.graph.out_edges(cue))
+
+def total_edges_fok(store, cue, target, query_time):
+    return len(store.graph.in_edges(cue)) + len(store.graph.out_edges(cue))
 
 # this function is specific to the AB- Task
 def create_edge(store, time, backlinks, representation, src, dst):
@@ -32,7 +47,7 @@ def create_edge(store, time, backlinks, representation, src, dst):
         store.store(time, backlinks, src, **{'goes_to_' + dst.lower(): dst, })
     elif representation == 'pairs':
         # two-level representation
-        store.store(time, backlinks, src + dst + str(time), first=src, second=dst)
+        store.store(time, backlinks, src + dst, first=src, second=dst)
     elif representation == 'types':
         # three levels (type=pairs is a shared child of AB and CD)
         store.store(time, backlinks, src + dst, first=src, second=dst, type='pairs')
@@ -61,14 +76,16 @@ def determine_query_parameters(representation):
 
 
 
-def test_model(fok_method):
+def test_model():
     act_decay_rate = [-0.42, -0.41]
     act_scale_factor = [0.5]
     act_max_steps = [2]
     act_capped = [False, True]
     backlinks = [True, False]
     representation = ['pairs', 'types']
-    paradigm = ['ABAB']
+    paradigm = ['ABAB', 'ABAD', 'ABCB', 'ABCD']
+    fok_method = ['total edges']
+
 
 
     generator = product(
@@ -79,36 +96,32 @@ def test_model(fok_method):
         backlinks,
         representation,
         paradigm,
+        fok_method
     )
-    for rate, scale, step, cap, link, rep, paradigm in generator:
+    for rate, scale, step, cap, link, rep, paradigm, fok_method in generator:
         print(', '.join([
             # 'decay rate = ' + str(rate),
             # 'scale factor = ' + str(scale),
             # 'max steps = ' + str(step),
             # 'capped = ' + str(cap),
-            # 'backlinks = ' + str(link),
+            'backlinks = ' + str(link),
             'representation = ' + rep,
-            'paradigm = ' + paradigm
+            'paradigm = ' + paradigm,
+            'fok_method = ' + fok_method
         ]))
 
         store = NetworkXKB(ActivationClass(rate, scale, step, cap))
         create_graph(store, link, rep, paradigm)
 
-    attr, cue, result_attr = determine_query_parameters(rep)
-    result = store.query(2, {attr: cue})[result_attr]
-    print('query first returns: ' + result)
+        attr, cue, result_attr = determine_query_parameters(rep)
+        result = store.query(2, {attr: cue})[result_attr]
+        print('query first returns: ' + result)
 
-    fok_function = determine_fok_function(fok_method)
-    fok = fok_function(store, cue, result, 2)
-    print('method = ' + fok_method + ' fok = ' + str(fok))
-
-
-def testy(tasks, fok_method):
-    for task in tasks:
-        if task == 'AB-':
-            for method in fok_method:
-                test_model(method)
+        fok_function = determine_fok_function(fok_method)
+        fok = fok_function(store, cue, result, 2)
+        print('fok = ' + str(fok))
 
 
 
-testy(['AB-'], ['cue', 'target'])
+
+test_model()
