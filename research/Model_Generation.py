6324cef9880d4cd5b823256214a70c88724291f0
Ruth Schlosser <rschlosser0205@gmail.com>
2020-06-05 11:09:33 -0700

    refactor to accommodate different fok calculations

--- research/Model_Generation.py
+++ research/Model_Generation.py
@@ -3,13 +3,27 @@ from itertools import product
 from research.rl_memory import ActivationClass, NetworkXKB
 
 
+
 # general method! but is it useful? returns activations right before the query
-def calculate_fok(store, cue, target, method, query_time):
+def determine_fok_function(method):
     if method == 'cue':
-        return store.get_activation(cue, (query_time - 0.0001))
+        return cue_fok
     elif method == 'target':
         # return the activation of what was returned by the query
-        return store.get_activation(target, (query_time - 0.0001))
+        return target_fok
+
+
+
+
+def cue_fok(store, cue, target, query_time):
+    return store.get_activation(cue, (query_time - 0.0001))
+
+
+
+
+def target_fok(store, cue, target, query_time):
+    return store.get_activation(target, (query_time - 0.0001))
+
 
 # this function is specific to the AB- Task
 def create_edge(store, time, backlinks, representation, src, dst):
@@ -23,32 +37,26 @@ def create_edge(store, time, backlinks, representation, src, dst):
         # three levels (type=pairs is a shared child of AB and CD)
         store.store(time, backlinks, src + dst, first=src, second=dst, type='pairs')
 
+
+
+
 # This function is specific to the AB- task
 def create_graph(store, backlinks, representation, paradigm):
     # paradigm is either 'ABAB', 'ABAD', 'ABCB', 'ABCD'
     create_edge(store, 0, backlinks, representation, 'A', 'B')
     create_edge(store, 1, backlinks, representation, paradigm[2], paradigm[3])
 
-# this function is specific to the AB- task
-def query_test(representation, store, fok_method):
-    # get from A to B in each representation and then print out the result
+
+
+
+
+def determine_query_parameters(representation):
     if representation == 'direct':
-        # specifying the value at the end of an edge protects from duplicate edges being added to the result map.
-        result = store.query(2, {'goes_to_b': 'B'})
-        final = result['node_id']
-        print('method = ' + fok_method + ' fok = ' + str(calculate_fok(store, 'B', final, fok_method, 2)))
+        return ('goes_to_b', 'B', 'node_id')
     elif representation == 'pairs':
-        result = store.query(2, {'first': 'A'}) # this should give AB or AD
-        final = result['second']
-        print('method = ' + fok_method + ' fok = ' + str(calculate_fok(store, 'A', final, fok_method, 2)))
+        return ('first', 'A', 'second')
     elif representation == 'types':
-        result = store.query(2, {'first': 'A'})  # this should give AB or AD
-        final = result['second']
-        print('method = ' + fok_method + ' fok = ' + str(calculate_fok(store, 'A', final, fok_method, 2)))
-
-
-    print('query first returns: ' + final)
-
+        return ('first', 'A', 'second')
 
 
 
@@ -62,6 +70,7 @@ def test_model(fok_method):
     representation = ['pairs', 'types']
     paradigm = ['ABAB']
 
+
     generator = product(
         act_decay_rate,
         act_scale_factor,
@@ -71,7 +80,6 @@ def test_model(fok_method):
         representation,
         paradigm,
     )
-
     for rate, scale, step, cap, link, rep, paradigm in generator:
         print(', '.join([
             # 'decay rate = ' + str(rate),
@@ -82,11 +90,17 @@ def test_model(fok_method):
             'representation = ' + rep,
             'paradigm = ' + paradigm
         ]))
+
         store = NetworkXKB(ActivationClass(rate, scale, step, cap))
         create_graph(store, link, rep, paradigm)
-        query_test(rep, store, fok_method)
-        print('')
 
+    attr, cue, result_attr = determine_query_parameters(rep)
+    result = store.query(2, {attr: cue})[result_attr]
+    print('query first returns: ' + result)
+
+    fok_function = determine_fok_function(fok_method)
+    fok = fok_function(store, cue, result, 2)
+    print('method = ' + fok_method + ' fok = ' + str(fok))
 
 
 def testy(tasks, fok_method):
@@ -94,7 +108,6 @@ def testy(tasks, fok_method):
         if task == 'AB-':
             for method in fok_method:
                 test_model(method)
-
 
 
 
